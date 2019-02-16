448c84f34d46a8355a943dfbe7b19f67899cdbec
Arebos <lent1510@gmail.com>
2019-02-16 22:18:23 +0900

    클라이언트 변경

---
+++ build/precache-manifest.c6686031cb9fae3d90cb0f44c53a0eb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4211b151bf1cf6505fddba46c57c149f",
+    "url": "/static/media/logo.4211b151.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5866a61964e4158a65df",
+    "url": "/static/js/main.5866a619.chunk.js"
+  },
+  {
+    "revision": "8925b6b2ceedbb1bea4c",
+    "url": "/static/js/1.8925b6b2.chunk.js"
+  },
+  {
+    "revision": "5866a61964e4158a65df",
+    "url": "/static/css/main.675b7046.chunk.css"
+  },
+  {
+    "revision": "fd95627e75176a467c36b79b88b19ab9",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca23a335c31c2499febf08c57089dd40.js"
+  "/precache-manifest.c6686031cb9fae3d90cb0f44c53a0eb3.js"
 );
 
 workbox.clientsClaim();
